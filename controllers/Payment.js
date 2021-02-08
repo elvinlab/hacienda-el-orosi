f618030830df6cea26dfe6e47e9c5fc6362f1396
JosestebanPorrasV <60045487+JosestebanPorrasV@users.noreply.github.com>
2021-02-07 21:10:56 -0600

    Modulo Payment , terminado
    listo

--- controllers/Payment.js
+++ controllers/Payment.js
@@ -3,9 +3,9 @@ const { response } = require("express");
 const { ObjectId } = require("mongodb");
 
 const registerSalaryCollaboratior = (req, res = response) => {
-  const { net_salary, salary, details } = req.body;
-
   if (req.user.role === "GENERAL_ROLE" || req.user.role === "RESOURCES_ROLE") {
+    const { net_salary, final_salary, details } = req.body;
+
     //valido el roll
     try {
       let payment = new Payment();
@@ -13,7 +13,7 @@ const registerSalaryCollaboratior = (req, res = response) => {
       payment.administrator = req.user.id; //seteo al modelo.
       payment.collaborator = req.params.id;
       payment.net_salary = net_salary;
-      payment.final_salary = salary;
+      payment.final_salary = final_salary;
       payment.details = details;
 
       payment.save();
@@ -35,87 +35,55 @@ const registerSalaryCollaboratior = (req, res = response) => {
   }
 };
 
-const registerSalaryAdministrator = (req, res = response) => {
-  if (req.user.role === "GENERAL_ROLE") {
-
-     try {
-      let payment = new Payment();
-      payment.administrator = req.user.id; //seteo al modelo.
-      payment.net_salary = net_salary;
-      payment.final_salary = salary;
-      payment.details = details;
-
-      payment.save();
-      return res.status(200).json({
-        status: "succes",
-        msg: "Pago realizado con exito",
-      });
-    } catch (error) {
-      return res.status(500).json({
-        status: "Error",
-        msg: "Porfavor contacte con el Administrador para mas información",
-      });
-  } 
-};
 const paymentsByCollaborator = (req, res = response) => {
-  const collaboratorId = req.params.id;
+  if (req.user.role === "GENERAL_ROLE" || req.user.role === "RESOURCES_ROLE") {
+    let page = undefined;
+    const collaboratorId = req.params.id;
+    if (
+      !req.params.page ||
+      req.params.page == 0 ||
+      req.params.page == "0" ||
+      req.params.page == null ||
+      req.params.page == undefined
+    ) {
+      page = 1;
+    } else {
+      page = parseInt(req.params.page);
+    }
+    const options = {
+      sort: { pay_day: "ascending" },
+      limit: 5,
+      page: page,
+    };
 
-  Payment.find({ collaborator: ObjectId(collaboratorId) })
-    .sort([["pay_day", "descending"]])
-    .exec((err, payments) => {
-      if (err) {
-        // Devolver resultado
-        return res.status(500).send({
-          status: "error",
-          msg: "Error en la peticion",
-        });
-      }
-
-      if (!payments) {
-        // Devolver resultado
-        return res.status(404).send({
-          status: "error",
-          msg: "No hay pagos por mostrar",
-        });
-      }
-
-      // Devolver resultado
-      return res.status(200).send({
-        status: "success",
-        payments,
-      });
-    });
-};
-
-const paymentsByAdministrator = (req, res = response) => {
-  if (req.user.role === "GENERAL_ROLE") {
-    const administratorId = req.params.id;
-
-    Payment.find({ administrator: ObjectId(administratorId) })
-      .sort([["pay_day", "descending"]])
-      .exec((err, payments) => {
+    Payment.paginate(
+      { collaborator: ObjectId(collaboratorId) },
+      options,
+      (err, payments) => {
         if (err) {
-          // Devolver resultado
           return res.status(500).send({
             status: "error",
-            msg: "Error en la peticion",
+            msg: "Error al hacer la consulta",
           });
         }
 
         if (!payments) {
-          // Devolver resultado
           return res.status(404).send({
             status: "error",
-            msg: "No hay pagos por mostrar",
+            msg: "Sin pagos registrados",
           });
         }
 
-        // Devolver resultado
-        return res.status(200).send({
+        return res.status(200).json({
           status: "success",
-          payments,
+          payments: {
+            payments: payments.docs,
+            count: payments.totalDocs,
+            totalPages: payments.totalPages,
+          },
         });
-      });
+      }
+    );
   } else {
     res.status(500).json({
       status: "Error",
@@ -123,12 +91,62 @@ const paymentsByAdministrator = (req, res = response) => {
     });
   }
 };
-const getPayments = (req, res = response) => {};
+
+const getPayments = (req, res = response) => {
+  if (req.user.role === "GENERAL_ROLE" || req.user.role === "RESOURCES_ROLE") {
+    let page = undefined;
+
+    if (
+      !req.params.page ||
+      req.params.page == 0 ||
+      req.params.page == "0" ||
+      req.params.page == null ||
+      req.params.page == undefined
+    ) {
+      page = 1;
+    } else {
+      page = parseInt(req.params.page);
+    }
+    const options = {
+      sort: { pay_day: "ascending" },
+      limit: 5,
+      page: page,
+    };
+
+    Payment.paginate({}, options, (err, payments) => {
+      if (err) {
+        return res.status(500).send({
+          status: "error",
+          msg: "Error al hacer la consulta",
+        });
+      }
+
+      if (!payments) {
+        return res.status(404).send({
+          status: "error",
+          msg: "Sin pagos registrados",
+        });
+      }
+
+      return res.status(200).json({
+        status: "success",
+        payments: {
+          payments: payments.docs,
+          count: payments.totalDocs,
+          totalPages: payments.totalPages,
+        },
+      });
+    });
+  } else {
+    res.status(500).json({
+      status: "Error",
+      msg: "No tienes permisos en la plataforma",
+    });
+  }
+};
 
 module.exports = {
   registerSalaryCollaboratior,
-  registerSalaryAdministrator,
   paymentsByCollaborator,
-  paymentsByAdministrator,
   getPayments,
 };
