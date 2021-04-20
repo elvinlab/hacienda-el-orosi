48b5d935289c569cfdba86936a3f87dc16d8f41a
Elvin González <60045534+elvinlab@users.noreply.github.com>
2021-04-19 21:31:47 -0600

    Optimizando api para subida de imagenes

--- routes/Animal.js
+++ routes/Animal.js
@@ -2,18 +2,24 @@ const { Router } = require("express");
 const { check } = require("express-validator");
 const { validate_fields } = require("../middlewares/Validate-fields");
 
+const multer = require("multer");
 const md_auth = require("../middlewares/Authenticated");
 const router = Router();
 
 const {
+  saveAnimalType,
+  getTypes,
+  removeType,
   register,
   update,
   changeStatus,
   changeNextDueDate,
   getAnimalByType,
   getAnimalByStatusAndType,
+  getAnimal,
   getAnimals,
-  findAnimalByPlateNumber,
+  uploadImgProfile,
+  uploadImgReg,
 } = require("../controllers/Animal.js");
 
 const {
@@ -34,6 +40,45 @@ const {
   deleteRegisterWeight,
 } = require("../controllers/Weight.js");
 
+const storage = multer.memoryStorage({
+  destination: function (req, file, callback) {
+    callback(null, "");
+  },
+});
+
+const upload = multer({ storage }).single("image");
+
+router.post(
+  "/subir/foto-de-perfil/:id",
+  [md_auth.authenticated, upload],
+  uploadImgProfile
+);
+router.post(
+  "/subir/foto-de-registro/:id",
+  [md_auth.authenticated, upload],
+  uploadImgReg
+);
+router.post(
+  "/registrar-tipo",
+  [
+    check("name", "Nombre  Requerido ").not().isEmpty(),
+    check("gender", "Genero requerido").not().isEmpty(),
+    validate_fields,
+  ],
+  md_auth.authenticated,
+  saveAnimalType
+);
+
+router.get(
+  "/tipo/:type/estado/:status/",
+  md_auth.authenticated,
+  getAnimalByStatusAndType
+);
+
+router.get("/ver-tipos", md_auth.authenticated, getTypes);
+
+router.delete("/remover-tipo/:id", md_auth.authenticated, removeType);
+
 router.post(
   "/registrar",
   md_auth.authenticated,
@@ -48,8 +93,6 @@ router.post(
 
     check("type_animal", "Tipo de animal  requerido").not().isEmpty(),
     check("status", "El estado es requerido").not().isEmpty(),
-    check("gender", "El genero es requerido").not().isEmpty(),
-
     validate_fields,
   ],
   register
@@ -74,6 +117,8 @@ router.put(
   update
 );
 
+router.get("/ver-animal/:id", md_auth.authenticated, getAnimal);
+
 router.put(
   "/actualizar-estado/:id",
   md_auth.authenticated,
@@ -91,20 +136,9 @@ router.put(
   changeNextDueDate
 );
 
-router.get("/todos/:page?", md_auth.authenticated, getAnimals);
+router.get("/todos/", md_auth.authenticated, getAnimals);
 
-router.get(
-  "/tipo/:type/:page?",
-  md_auth.authenticated,
-  getAnimalByType
-);
-router.get(
-  "/tipo/:type/estado/:status/:page?",
-  md_auth.authenticated,
-  getAnimalByStatusAndType
-);
-
-router.get("/buscar/:id", md_auth.authenticated, findAnimalByPlateNumber);
+router.get("/tipo/:type", md_auth.authenticated, getAnimalByType);
 
 //Rutas de MIlk
 router.post(
