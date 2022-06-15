6605e4072dfebeb07a23aa4d50a29f439f01005d
Elvin González <60045534+elvinlab@users.noreply.github.com>
2022-06-15 15:07:11 -0600

    fix deploy 3

--- Express_server.js
+++ Express_server.js
@@ -15,11 +15,10 @@ app.use(cors());
 
 app.use(express.json());
 
-//Static file declaration
-app.use(express.static(path.join(__dirname, '/public')));
-//build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.use('/api', require('./routes/User.js'));
